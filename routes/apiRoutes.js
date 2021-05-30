a77570f3d92a833ce43f0e298750dfcaf2615426
Doro Jacques <doro.jacques.soromou@innovationstechnologique.com>
2021-05-30 00:01:03 -0300

    Structure backend

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,11 @@ const express = require("express")
 const apiRouter = express.Router()
 
 const userController = require('../Controller/userController')
+const garageController = require('../Controller/garageController')
+const evenementController = require('../Controller/evenementController')
+const serviceController = require('../Controller/serviceController')
+const travauxController = require('../Controller/travauxController')
+const vehiculeController = require('../Controller/vehiculeController')
 
 apiRouter.post('/signup',userController.signupUser)
 apiRouter.get('/users',userController.fetchUsers)
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes/apiRoutes')
 
@@ -14,4 +14,4 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/',apiRouter)
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(` app listening on port ${PORT}!`));
